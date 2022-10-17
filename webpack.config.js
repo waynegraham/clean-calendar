a7987bf2d5fd96a8dd0b4e0b32e4aa0b3cf16bdd
Wayne Graham <wgraham@clir.org>
2022-10-17 11:56:58 -0400

    Initial bootstrap build

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './src/js/main.js',
+  plugins: [new miniCssExtractPlugin()],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // loader: 'style-loader'
+            loader: miniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
